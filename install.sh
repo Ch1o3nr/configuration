45e41b7b12cfcf85542a47f3d106cfbb05ce055b
Ch1o3nr <ch1o3nr@gmail.com>
2021-06-13 19:46:23 +0800

    test

---
+++ install.sh
@@ -0,0 +1,13 @@
+g#!/bin/bash
+sudo yum install git -y || echo "$LINENO: failed";
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || echo "$LINENO: failed";
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || echo "$LINENO: failed";
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || echo "$LINENO: failed";
+cp ~/.zshrc ~/.zshrc.bak || echo "$LINENO: failed";
+sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting sudo)/g" ~/.zshrc || echo "$LINENO: failed";
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k || echo "$LINENO: failed";
+sed -i "s/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"/g" ~/.zshrc || echo "$LINENO: failed";
+sudo yum install emacs -y || echo "$LINENO: failed";
+curl ch1o3nr.com/asdf.bak > ~/.emacs.d/init.el || echo "$LINENO: failed";
+sudo cp -r ~/.emacs.d /root || echo "$LINENO: failed";
+echo -e '\n#alias\nalias em="emacs"\n#alias END' >> ~/.zshrc || echo "$LINENO: failed";
