a82f9cdfa542c5146d2fc88b1c6ad481a502b4c3
Ch1o3nr <ch1o3nr@gmail.com>
2021-06-30 12:23:03 +0800

    test

--- penv_yum.sh
+++ penv_yum.sh
@@ -1,4 +1,4 @@
 yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel || echo "$LINENO: failed";
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv || echo "$LINENO: failed";
-echo -e '\n#penv\nexport PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init -)"\n#penv END' >> ~/.zshrc || echo "$LINENO: failed";
+echo -e '\n#penv\nexport PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init -- path)"\n#penv END' >> ~/.zshrc || echo "$LINENO: failed";
 exec $SHELL -l || echo "$LINENO: failed";
