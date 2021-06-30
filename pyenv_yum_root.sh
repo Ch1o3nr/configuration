bf84fb8d6f26e3ef158054a6cefeafb3ddf46da9
Ch1o3nr <ch1o3nr@gmail.com>
2021-06-30 15:13:38 +0800

    test

--- pyenv_yum_root.sh
+++ pyenv_yum_root.sh
@@ -1,4 +1,4 @@
-sudo dnf groupinstall -y "Development Tools" || echo "$LINENO: failed";
-sudo dnf install -y zlib zlib-devel bzip2-devel openssl-devel sqlite-devel readline-devel || echo "$LINENO: failed";
+dnf groupinstall -y "Development Tools" || echo "$LINENO: failed";
+dnf install -y zlib zlib-devel bzip2-devel openssl-devel sqlite-devel readline-devel || echo "$LINENO: failed";
 echo -e '\n#pyenv\nexport PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init --path)"\n#pyenv END' >> ~/.zshrc || echo "$LINENO: failed";
 exec $SHELL -l || echo "$LINENO: failed";
