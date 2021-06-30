103872d884130435d5c36ff10329639961586860
Ch1o3nr <ch1o3nr@gmail.com>
2021-06-30 18:07:58 +0800

    test

--- push_git.sh
+++ push_git.sh
@@ -1,3 +1,12 @@
-git add . || echo "$LINENO: failed";
-git commit -m "$1" || echo "$LINENO: failed";
-git push -f origin master || echo "$LINENO: failed";
+#!/usr/bin/expect
+set commit test
+set passwd arst
+set timeout 30
+
+spawn git add .
+spawn git commit -m $commit
+spawn git push -f origin master
+expect {
+    "*id_rsa':" { send "$passwd\r" }
+}
+interact
