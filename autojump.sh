24afd42d82e074c8c87bd326d3b470e35ee11860
Ch1o3nr <ch1o3nr@gmail.com>
2021-11-26 21:03:43 +0800

    test

--- autojump.sh
+++ autojump.sh
@@ -1,4 +1,4 @@
 cd ~/Downloads && git clone git://github.com/wting/autojump.git || echo "$LINENO: failed";
-cd ./autojump && ./install.py || echo "$LINENO: failed";
-echo -e '\n#autojump\n        [[ -s /home/user/.autojump/etc/profile.d/autojump.sh ]] && source /home/user/.autojump/etc/profile.d/autojump.sh\n\n        autoload -U compinit && compinit -u\n#autojump END' >> ~/.zshrc || echo "$LINENO: failed";
+cd ~/Downloads/autojump && ./install.py && cd && rm -rf ~/Downloads/autojump|| echo "$LINENO: failed";
+echo -e '\n#autojump\n        [[ -s $HOME/.autojump/etc/profile.d/autojump.sh ]] && source /home/user/.autojump/etc/profile.d/autojump.sh\n\n        autoload -U compinit && compinit -u\n' >> ~/.zshrc || echo "$LINENO: failed";
 exec $SHELL -l || echo "$LINENO: failed";
